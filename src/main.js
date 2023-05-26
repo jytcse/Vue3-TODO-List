f48e06ad67786afc984495116027ea50f400418d
jimmy <jimmy20031111@gmail.com>
2023-05-26 19:04:46 +0800

    Initial project
    create project
    import tailwindcss

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import "./style/index.css"; //import tailwindcss
+createApp(App).use(router).mount('#app')
