bdd5cf25d80ee1e5449c614d96ff34602c541910
jimmy <jimmy20031111@gmail.com>
2023-06-04 15:01:57 +0800

    Fix: RouterGuard can't redirect properly

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -16,16 +16,39 @@ const axiosInstance = axios.create({
 const app = createApp(App);
 app.use(VueAxios, axios);
 app.provide("axios", axiosInstance);
+const isLoggedIn = ref(await checkLogin());
+app.provide("isLoggedIn", isLoggedIn.value);
 app.use(router);
 app.mount('#app');
 
 //驗證是否有權限能夠訪問頁面
-router.beforeEach((to)=>{
-    if(to.name == 'about') return true;
-    axiosInstance.get('/check_login')
-    .then( (response) => {
-        if(response.data["success"]) return true;
-        else return router.push({name:'home'});
-    })
-    .catch( (error) => console.log(error));
+router.beforeEach(async (to)=>{
+    isLoggedIn.value = await checkLogin();
+    // 如果使用者已登錄且試圖訪問home頁，重導向到dashboard
+    if (to.name === 'home' && isLoggedIn.value) {
+        return router.push({name:'dashboard'});
+    } 
+    // 如果使用者未登錄且試圖訪問非home或about頁，將重導向到home頁
+    else if ((to.name !== 'home' && to.name !== 'about') && !isLoggedIn.value) {
+        return router.push({name:'home'}); 
+    }
+    else return true; 
 });
+
+/**
+ * 檢查登入狀態
+ * @function checkLogin
+ * @returns {Promise<Boolean>}
+ */
+ function checkLogin() {
+    return new Promise((resolve, reject) => {
+      axiosInstance.get('/check_login')
+        .then(function (response) {
+          resolve(response.data["success"]);
+        })
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  }
